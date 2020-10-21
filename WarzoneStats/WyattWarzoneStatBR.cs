560e3537e1fefb6d9bd422d7d17712e562c3e28d
Wyatt Brown <wyatt.brown23@gmail.com>
2020-10-21 16:43:09 -0600

    getting more data

--- WarzoneStats/WyattWarzoneStatBR.cs
+++ WarzoneStats/WyattWarzoneStatBR.cs
@@ -7,6 +7,14 @@ namespace WarzoneStats
 {
     public class WyattWarzoneStatBR
     {
+        public int wins { get; set; }
         public int kills { get; set; }
+        public float kdRatio { get; set; }
+        public int downs { get; set; }
+        public int topTen { get; set; }
+        public int contracts { get; set; }
+        public int revives { get; set; }
+        public int score { get; set; }
+        public int deaths { get; set; }
     }
 }
